613bd6111830e8ddc9183115e639857410ac0ed6
github username <github email address>
2019-07-09 13:51:03 -0400

    added categgory names, working on adding questions to DOM

--- trivia-app-backend/app/controllers/questions_controller.rb
+++ trivia-app-backend/app/controllers/questions_controller.rb
@@ -1,14 +1,15 @@
 class QuestionsController < ApplicationController
 
 
-  def index 
-    @questions = Question.all 
+  def index
+    @questions = Question.all
     render json: (@questions)
-  end 
+  end
 
-  def show 
-    @question = Question.find_by(params[:id])
-  end 
+  def show
+    @question = Question.find(params[:id])
+    render json: (@question)
+  end
 
   def create
   end
--- trivia-app-backend/app/models/category.rb
+++ trivia-app-backend/app/models/category.rb
@@ -1,3 +1,31 @@
 class Category < ApplicationRecord
-    has_many :questions 
+    has_many :questions
+
+    def easy_questions
+      self.questions.select do |question|
+          question.difficulty == "easy"
+      end
+    end
+
+    def medium_questions
+      self.questions.select do |question|
+          question.difficulty == "medium"
+      end
+    end
+
+    def hard_questions
+      self.questions.select do |question|
+          question.difficulty == "hard"
+      end
+    end
+
+    def round_of_questions
+      questions = []
+      questions << self.easy_questions.sample(2)
+      questions << self.medium_questions.sample(2)
+      questions << self.hard_questions.sample
+      questions.flatten
+    end
+
+
 end
